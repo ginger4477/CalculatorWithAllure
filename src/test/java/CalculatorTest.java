db472b1bc7ffafaed44d5bfa8c8a71e433d06cc3
ginger4477 <gyongyverdeak@gmail.com>
2023-03-19 17:24:18 +0100

    complete

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,35 +1,108 @@
 import io.qameta.allure.*;
 import org.example.Calculator;
-import org.junit.jupiter.api.Assertions;
+
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@Epic("Epic1")
+import static org.junit.jupiter.api.Assertions.assertNull;
+
 public class CalculatorTest {
 
     @Test
-    @Story("User story1")
-    @Description("Discription related to testAdd test")
-    @Severity(SeverityLevel.NORMAL)
-    public void testAdd() {
-        Calculator calculator = createCalc();
-        Assertions.assertEquals(5, calculator.Add(2, 3));
+    public void testIsNumberValid() {
+        // test isNumber method with the value: 11
+        Float expected = 11.0f;
+        Float actual = Calculator.isNumber("11");
+        assertEquals(expected, actual, 0.001f);     //a 0.001f pedig a lebegőpontos értékek általános hibahatárát jelöli.
+
     }
-
-
-    @Step("Calculator initialization")
-    private Calculator createCalc() {
-        return new Calculator();
-    }
-
 
     @Test
-    @Story("User story2")
-    public void testAddWithNegativeNumbers() {
-        Calculator calculator = createCalc();
-        Assertions.assertEquals(-5, calculator.Add(-2, -3));
+    public void testIsNumberNotValid() {
+        // test isNumber method with the value: "xyz"
+        // Given
+        String input = "xyz";
+
+        // When
+        Float result = Calculator.isNumber(input);
+
+        // Then
+        assertNull(result);
     }
 
+    @Test
+    public void testIsValidOperatorValid() {
+        // test isValidOperator method with the value: "*"
+        // Given
+        String operator = "*";
 
+        // When
+        boolean result = Calculator.isValidOperator(operator);
+
+        // Then
+        assertTrue(result);
+    }
+
+    @Test
+    public void testCalcPlus() {
+        // test addition using the calc method with the values: "+", 5, 7
+        // Given
+        String operator = "+";
+        Float a = 5.0f;
+        Float b = 7.0f;
+
+        // When
+        Float result = Calculator.calc(operator, a, b);
+
+        // Then
+        assertEquals(12.0f, result, 0.0001f);
+    }
+
+    @Test
+    public void testCalcMinus() {
+        // test subtraction using the calc method with the values: "-", 5, 7
+        // Given
+        String operator = "-";
+        Float a = 5.0f;
+        Float b = 7.0f;
+
+        // When
+        Float result = Calculator.calc(operator, a, b);
+
+        // Then
+        assertEquals(-2.0f, result, 0.0001f);
+    }
+
+    @Test
+    public void testCalcMultiply() {
+        // test multiplication using the calc method with the values: "*", 5, 7
+        // Given
+        String operator = "*";
+        Float a = 5.0f;
+        Float b = 7.0f;
+
+        // When
+        Float result = Calculator.calc(operator, a, b);
+
+        // Then
+        assertEquals(35.0f, result, 0.0001f);
+    }
+
+    @Test
+    public void testCalcDivision() {
+        // test division using the calc method with the values: "/", 5, 7
+        // Given
+        String operator = "/";
+        Float a = 5.0f;
+        Float b = 7.0f;
+
+        // When
+        Float result = Calculator.calc(operator, a, b);
+
+        // Then
+        assertEquals(0.7142857f, result, 0.0001f);
+    }
 
 }
